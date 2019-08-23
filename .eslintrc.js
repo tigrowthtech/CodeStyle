a32bb46161d4160cd0879578355c95a6c5696af8
Jonathan Silva <jonathan.jgs@gmail.com>
2019-08-23 18:17:27 -0300

    ajustes de eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: ["airbnb-base"],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-    },
-    rules: {
-        indent: ["error", 2],
-        "no-multiple-empty-lines": [1, { max: 1 }],
-        "class-methods-use-this": "off",
-        "no-param-reassign": "off",
-        "comma-dangle": [
-            "error",
-            {
-                arrays: "never",
-                objects: "always",
-            },
-        ],
-        "no-unused-vars": [
-            "error",
-            {
-                argsIgnorePattern: "next",
-            },
-        ],
-    },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-multiple-empty-lines': [1, { max: 1 }],
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'always',
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: 'next',
+      },
+    ],
+  },
 };
