2bd137de041984afbf321b1b3b999ba3b1f7d20e
Jonathan Silva <jonathan.jgs@gmail.com>
2019-08-23 18:06:23 -0300

    adjusts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: ["airbnb-base"],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-    },
-    rules: {
-        indent: ["error", 2],
-        "no-multiple-empty-lines": [1, { max: 1 }],
-        "class-methods-use-this": "off",
-        "no-param-reassign": "off",
-        "no-unused-vars": [
-            "error",
-            {
-                argsIgnorePattern: "next",
-            },
-        ],
-    },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-multiple-empty-lines': [1, { max: 1 }],
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'comma-dangle': ['error'],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: 'next',
+      },
+    ],
+  },
 };
--- code_examples/User.js
+++ code_examples/User.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
 export default class User {
   constructor(name, age, cpf) {
     this.name = name;
     this.age = age;
     this.cpf = cpf;
+  }
+
+  sayMyName() {
+    console.log('Name:');
+
+    console.log(`My name is: ${this.name}`);
   }
 
   canDrink() {
